f35deff8ca1502b407892733c482cea154dc4236
Yudai Jinno <jinno.yudai@classmethod.jp>
2024-10-24 08:01:08 +0900

    Initial commit

---
+++ test/auto_scaling.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AutoScaling from '../lib/auto_scaling-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/auto_scaling-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AutoScaling.AutoScalingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
